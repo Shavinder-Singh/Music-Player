54d596772af2544fb48aa930e38759a3a35ac459
Shavinder <singhshub3004@gmail.com>
2024-06-13 17:03:29 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let songs = [
+    {
+        name: 'Brown Rang',
+        path: 'audio/Brown_Rang.mp3',
+        artist: "Yo Yo Honey Singh",
+        cover: 'images/honeysingh.jpeg'
+    },
+    {
+        name: 'That Girl',
+        path: 'audio/That_Girl_1.mp3',
+        artist: "'Amrinder Gill",
+        cover: 'images/amrindergill.jpg'
+    },
+    {
+        name: 'Dua',
+        path: 'audio/Dua.mp3',
+        artist: "'Sharry Maan",
+        cover: 'images/sharryman.jpg'
+    },
+    {
+        name: 'That Girl',
+        path: 'audio/That_Girl_1.mp3',
+        artist: "'Amrinder Gill",
+        cover: 'images/amrindergill.jpg'
+    },
+
+]
